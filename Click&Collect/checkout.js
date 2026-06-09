dfb4b53d32ee1b93b22fc119ed92d465b7094e90
Leonardo de Bortoli <theleobortoli@gmail.com>
2026-06-08 21:06:51 -0300

    Add files via upload
    Adição de campos no formulário de entrega, com 5 input text e 1 select com todas as UFs, além da validação ao dar submit (ainda incompleta, só verifica se o campo está vazio)

--- Click&Collect/checkout.js
+++ Click&Collect/checkout.js
@@ -46,7 +46,6 @@ function validarCEP(cep){
     return /^[0-9]{8}$/.test(cep);
 }
 
-
 async function buscarCEP(){
     const cep = document.getElementById("cep").value;
     if(!validarCEP(cep)){
@@ -221,16 +220,129 @@ document
     .addEventListener("click", buscarCEP);
 
 });
+this.validarCamposForm = validarCamposForm;
+function validarCamposForm(event)
+{
+    const enderecoCampo = document.getElementById('endereco');
+    const cepCampo = document.getElementById('cep');
+    const bairroCampo = document.getElementById('bairro');
+    const numCampo = document.getElementById('num');
+    const cidadeCampo = document.getElementById('cidade');
+    const ufCampo = document.getElementById('uf');
+
+    if (enderecoCampo.value === null || enderecoCampo.value === "")
+    {
+        enderecoCampo.focus()
+        enderecoCampo.value = "";
+        alert("Campo 'ENDEREÇO' não pode estar vazio.");
+        event.preventDefault();
+        return false;
+    }
+
+    if (cepCampo.value === null || cepCampo.value === "")
+    {
+        cepCampo.focus()
+        cepCampo.value = "";
+        alert("Campo 'CEP' não pode estar vazio.");
+        event.preventDefault();
+        return false;
+    }
+
+    if (bairroCampo.value === null || bairroCampo.value === "")
+    {
+        bairroCampo.focus()
+        bairroCampo.value = "";
+        alert("Campo 'BAIRRO' não pode estar vazio.");
+        event.preventDefault();
+        return false;
+    }
+
+    if (numCampo.value === null || numCampo.value === "")
+    {
+        numCampo.focus()
+        numCampo.value = "";
+        alert("Campo 'NÚMERO' não pode estar vazio.");
+        event.preventDefault();
+        return false;
+    }
+
+    if (cidadeCampo.value === null || cidadeCampo.value === "")
+    {
+        cidadeCampo.focus()
+        cidadeCampo.value = "";
+        alert("Campo 'CIDADE' não pode estar vazio.");
+        event.preventDefault();
+        return false;
+    }
+
+    if (ufCampo.value === null || ufCampo.value === "")
+    {
+        ufCampo.focus()
+        alert("Campo 'UF' não pode estar vazio.");
+        event.preventDefault();
+        return false;
+    }
+    
+    return true;
+};
 
 entrega.addEventListener("click",() => {
     entrega.classList.add("ativa");
     retirar.classList.remove("ativa");
     document.getElementById("resultado").innerHTML = "";
     info.innerHTML = `
-    <form class="opcao2">
+    <form class="opcao2" id="formEntrega" onsubmit="validarCamposForm(event);">
         <h2>Entrega no local</h2>
-        <label for="endereco">Digite o endereço de entrega:</label>
-        <input type="text" id="endereco" name="endereco" placeholder="Ex.: Rua sete, 123"> 
+            <div id="fluxoBuscar">
+                <div class="form-group"><label for="endereco">Endereço:</label><input type="text" id="endereco" placeholder="Av. Presidente Vargas"></div>
+                <div class="form-group"><label for="cep">CEP:</label><input type="text" id="cep" placeholder="91440567"></div>
+                <div class="form-group"><label for="bairro">Bairro:</label><input type="text" id="bairro" placeholder="Marechal Rondon"></div>
+                <div class="form-group"><label for="num">Nº:</label><input type="text" id="num" placeholder="12"></div>
+                <div class="form-group"><label for="cidade">Cidade:</label><input type="text" id="cidade" placeholder="Novo Hamburgo"></div>
+                <div class="form-group"><label for="uf">UF:</label><select name="uf" id="uf">
+                <option value="" disabled>--Selecione uma UF--</option>
+                <optgroup label="Sul">
+                    <option value="pr">Paraná</option> 
+                   <option value="rs">Rio Grande do Sul</option>
+                   <option value="sc">Santa Catarina</option> 
+                </optgroup>
+                <optgroup label="Sudeste">
+                   <option value="es">Espírito Santo</option>
+                   <option value="mg">Minas Gerais</option> 
+                   <option value="rs">Rio de Janeiro</option>  
+                   <option value="sp">São Paulo</option>  
+                </optgroup>
+                <optgroup label="Centro-Oeste">
+                   <option value="df">Distrito Federal</option>  
+                   <option value="go">Goiás</option>
+                   <option value="mt">Mato Grosso</option> 
+                   <option value="ms">Mato Grosso do Sul</option>  
+                </optgroup>
+                <optgroup label="Nordeste">
+                   <option value="al">Alagoas</option>  
+                   <option value="ba">Bahia</option>
+                   <option value="ce">Ceará</option> 
+                   <option value="ma">Maranhão</option>  
+                   <option value="pb">Paraíba</option>  
+                   <option value="pe">Pernambuco</option>
+                   <option value="pi">Piauí</option> 
+                   <option value="rn">Rio Grande do Norte</option>  
+                   <option value="se">Sergipe</option>  
+                </optgroup>
+                <optgroup label="Norte">
+                   <option value="ac">Acre</option>  
+                   <option value="ap">Amapá</option>
+                   <option value="am">Amazonas</option> 
+                   <option value="pa">Pará</option>  
+                   <option value="ro">Rondônia</option>  
+                   <option value="rr">Roraima</option>
+                   <option value="to">Tocantins</option> 
+                   <option value="rn">Rio Grande do Norte</option>  
+                   <option value="se">Sergipe</option>  
+                </optgroup>
+                </select></div>
+                <input type="submit" value="Enviar" class="botaoSubmit" />
+            </div>
     </form>
     `;
 });
